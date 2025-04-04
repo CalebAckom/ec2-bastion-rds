52e4ae1e0b5aab8205506552808732f64c4add95
CalebAckom <calebackom@gmail.com>
2025-04-04 15:28:15 +0000

    iac: Using S3 for state locking and used workflow_call event for GitHub Action

--- init/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "s3_bucket_name" {
-  description = "S3 Bucket Name"
-  default = "bastion-terraform-test-s3"
-}
-
-variable "dynamodb_table_name" {
-  description = "DynamoDB Table Name"
-  default = "bastion-terraform-test-dynamodb"
-}
