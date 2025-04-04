02f168189424a4cbdf652ad5ddcc9ac740493d1b
CalebAckom <calebackom@gmail.com>
2025-04-04 16:44:30 +0000

    iac: Adding key pair and associating the security group

--- ec2.tf
+++ ec2.tf
@@ -2,6 +2,8 @@ resource "aws_instance" "bastion" {
   ami = "ami-0e9085e60087ce171"
   instance_type = "t2.micro"
   subnet_id = aws_subnet.public.id
+  key_name = "ci_cd"
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
 
   tags = {
     Name = "bastion-test-ec2"
